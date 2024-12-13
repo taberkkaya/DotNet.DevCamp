98f03f519b2222d994c71a29919515a3c2569723
taberkkaya <taberkkaya@gmail.com>
2024-12-13 09:29:12 +0300

    asp.net partial views

--- first-proj/obj/Debug/net9.0/first-proj.AssemblyInfo.cs
+++ first-proj/obj/Debug/net9.0/first-proj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("first-proj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9483b7ec64d846dd48b0924fda3b68a9c762d58a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b5a629a795d57e06f0fea9db9ae0fef8e87f652")]
 [assembly: System.Reflection.AssemblyProductAttribute("first-proj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("first-proj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
