e473613f6da1b15d468a4c9f2e6cc091f47b34f0
taberkkaya <taberkkaya@gmail.com>
2024-12-24 22:48:29 +0300

    blogApp proj created

--- first-proj/obj/Debug/net9.0/first-proj.AssemblyInfo.cs
+++ first-proj/obj/Debug/net9.0/first-proj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("first-proj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15ccede51047aadae89132b628ec89d42ce7ae78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+825dc398b791deb42058f98016f182e25aba15cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("first-proj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("first-proj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
