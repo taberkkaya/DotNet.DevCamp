11d76aaf2dbf236dead49fac09a8d27d955f0ae0
taberkkaya <taberkkaya@gmail.com>
2025-01-05 11:35:17 +0300

    ef core identity section starter

---
+++ IdentityApp/Controllers/UsersController.cs
@@ -0,0 +1,55 @@
+using IdentityApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace IdentityApp.Controllers;
+
+public class UsersController : Controller
+{
+    private UserManager<IdentityUser> _userManager;
+
+    public UsersController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(_userManager.Users);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateUserViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = new IdentityUser
+            {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+
+            foreach (IdentityError err in result.Errors)
+            {
+                ModelState.AddModelError("", err.Description);
+            }
+        }
+
+        return View(model);
+    }
+}
